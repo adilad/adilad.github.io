04296bb00b6cbf02abf57b425bf612f4553d319a
Aditya Lad <ladaditya@Adityas-MacBook-Pro.local>
2025-09-11 22:52:52 -0400

    automatic year

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+import os
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
+
+
+@app.context_processor
+def inject_current_year():
+    return {"current_year": datetime.now().year}
 
 @app.route('/')
 def index():
